2d4efd43272a0c932447f4f41d93d541dc962ae8
Rahul Krishna <rahulkrishna.web@gmail.com>
2024-11-08 16:15:34 +0530

    revert

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -1,14 +1,9 @@
-import {useAppBridge} from '@shopify/app-bridge-react';
+
 
 const Dashboard = () => {
-    const app = useAppBridge();
-    function generateBlogPost() {
-        // Handle generating
-        app.toast.show('Blog post template generated');
-      }
+    
     return(
         <div>Dashboard
-            <button onClick={generateBlogPost}>Generate Blog Post</button>
         </div>
     )
 }
