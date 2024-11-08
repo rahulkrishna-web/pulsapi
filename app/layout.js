fdee4561a9c010d687f218d098d2bc1d1677f811
Rahul Krishna <rahulkrishna.web@gmail.com>
2024-11-08 15:31:50 +0530

    try fix app bridge

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,31 @@
 'use client';
+import { useSearchParams } from 'next/navigation';
 import { AppProvider as PolarisProvider } from '@shopify/polaris';
 import enTranslations from '@shopify/polaris/locales/en.json';
 
 import '@shopify/polaris/build/esm/styles.css';
 
 export default function Layout({ children }) {
+  const searchParams = useSearchParams();
+  const shop = searchParams.get('shop');
+
   return (
     <html>
       <head>
-        <meta name="shopify-api-key" content={process.env.SHOPIFY_API_KEY} />
-        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
+        <title>Meta</title>
+        {shop && (
+          <>
+          <meta name="shopify-api-key" content={process.env.SHOPIFY_API_KEY} />
+          <meta name="shopify-shop" content={shop} />
+          <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
+          </>
+        )}
       </head>
-      <body>
-        <PolarisProvider i18n={enTranslations}>
-          {children}
-        </PolarisProvider>
-      </body>
+    <body>
+    <PolarisProvider i18n={enTranslations}>
+        {children}
+    </PolarisProvider>
+    </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,89 +1,17 @@
 'use client';
-
-import React, { useState, useEffect, useCallback } from 'react';
-import Image from 'next/image';
-import { Button, Card, Page, TextField, BlockStack, InlineStack, Text } from '@shopify/polaris';
-import TestBridge from './components/TestBridge';
+import { useSearchParams } from 'next/navigation';
+import React, { useState } from 'react';
+import StoreConnectForm from './components/storeConnectForm';
 
 export default function Home() {
-  const [storeUrl, setStoreUrl] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const searchParams = useSearchParams();
+  const shop = searchParams.get('shop');
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const handleStoreUrlChange = useCallback(
-    (newValue) => setStoreUrl(newValue),
-    [],
-  );
-
-  const connectStore = async () => {
-    if (!storeUrl) {
-      return console.log("No store URL entered.");
-    }
-
-    setLoading(true);
-    setError(null);
-
-    const apiKey = process.env.SHOPIFY_API_KEY;
-    const redirectUri = process.env.NEXT_PUBLIC_REDIRECT_URI;
-    const scopes = process.env.NEXT_PUBLIC_SHOPIFY_SCOPES;
-    const state = Math.random().toString(36).substring(2);
-
-    try {
-      const data = { shop_url: storeUrl, state };
-
-      const response = await fetch('https://eventsguy.clyrix.com/api/store-shop', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data),
-      });
-
-      const result = await response.json();
-
-      if (result.status === 'created' || result.status === 'exists') {
-        const authUrl = `https://${storeUrl}/admin/oauth/authorize?client_id=${apiKey}&scope=${scopes}&redirect_uri=${redirectUri}&state=${state}`;
-        window.location.href = authUrl;
-      } else {
-        console.log("Unexpected status: ", result.status);
-      }
-    } catch (error) {
-      console.error("Error during request:", error);
-      setError('Failed to connect the store. Please try again later.');
-    } finally {
-      setLoading(false);
-    }
-  };
-
   return (
-    <Page>
-      
-      <Card sectioned>
-            <BlockStack gap="200">
-              <Text variant="headingXl" as="h1">
-                Connect your store
-              </Text>
-              <BlockStack gap="400">
-                <TextField
-                  label="Store name"
-                  placeholder="Enter Store URL"
-                  value={storeUrl}
-                  onChange={handleStoreUrlChange}
-                />
-              </BlockStack>
-              <InlineStack align="end">
-              <Button
-                  primary
-                  onClick={connectStore}
-                  disabled={loading}
-                >
-                  {loading ? 'Connecting...' : 'Connect'}
-                </Button>
-                </InlineStack>
-              </BlockStack>
-          </Card>
-
-        {error && <p className="text-red-500 mt-4">{error}</p>}
-      
-    </Page>
+    <>
+    {shop}
+    <StoreConnectForm />
+    </>
   );
 }
